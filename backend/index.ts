e43c95fe4595be3d7af83247c0f759ab926e1abd
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-11-03 19:35:28 +0200

    add project basic structure

---
+++ backend/index.ts
@@ -0,0 +1,39 @@
+import fastify from 'fastify';
+const server = fastify({ logger: true });
+
+server.route({
+  method: 'GET',
+  url: '/',
+  schema: {
+    // request needs to have a querystring with a `name` parameter
+    querystring: {
+      name: { type: 'string' },
+    },
+    // the response needs to be an object with an `hello` property of type 'string'
+    response: {
+      200: {
+        type: 'object',
+        properties: {
+          hello: { type: 'string' },
+        },
+      },
+    },
+  },
+  // this function is executed for every request before the handler is executed
+  preHandler: async (request, reply) => {
+    // E.g. check authentication
+  },
+  handler: async (request, reply) => {
+    return { hello: 'world' };
+  },
+});
+
+const start = async () => {
+  try {
+    await server.listen(3000);
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+start();
