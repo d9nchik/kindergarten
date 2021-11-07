6293b54e4cc3d17c1e6255c9c6bd82b7e5c833b5
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-11-07 20:08:47 +0200

    add user class to frontend

---
+++ frontend/src/utils/user.ts
@@ -0,0 +1,45 @@
+export const SERVER_URL = 'localhost:3000';
+
+class User {
+  protected jwt_token: string = '';
+  protected status_array: string[] = [];
+  protected organizer_company: number[] = [];
+
+  constructor(user: User | null = null) {
+    if (!user) {
+      return;
+    }
+    this.jwt_token = user.jwt_token;
+  }
+  public isLogin(): boolean {
+    return !!this.jwt_token;
+  }
+
+  public async login(email: string, password: string) {
+    const body = JSON.stringify({ login: email, password });
+    const result = await fetch(`${SERVER_URL}/login`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body,
+    });
+    if (!result.ok) {
+      throw new Error('check your credentials');
+    }
+    const payload = (await result.json()) as {
+      token: string;
+      statusArray: string[];
+      organizerArray: number[];
+    };
+    this.jwt_token = payload.token;
+    this.organizer_company = payload.organizerArray;
+    this.status_array = payload.statusArray;
+  }
+
+  public logout() {
+    this.jwt_token = '';
+    this.status_array = [];
+    this.organizer_company = [];
+  }
+}
+
+export default User;
