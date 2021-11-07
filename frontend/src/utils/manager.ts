6fc1ace599a48a462ac431c5cf9aa257bbffb667
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-11-07 20:10:40 +0200

    add manager class to frontend

---
+++ frontend/src/utils/manager.ts
@@ -0,0 +1,98 @@
+import User, { SERVER_URL } from './user';
+
+interface EventProps {
+  id: number;
+  name: string;
+  date: number;
+  startTime: string;
+  endTime: string | null;
+  price: number;
+  minParticipantsCount: number;
+  detailedInfo: string | null;
+}
+interface ShortOrganizerInfo {
+  id: number;
+  companyName: string;
+}
+
+interface OrganizerInformation {
+  organizerID: number;
+  name: string;
+  address: string;
+  phone_number: string;
+  site: string | null;
+  mail: string;
+  detailed_info: string | null;
+}
+
+class Manager extends User {
+  public async notifyParents(): Promise<boolean> {
+    return true;
+  }
+
+  public async getNotSelectedEvents(): Promise<EventProps[]> {
+    const response = await fetch(`${SERVER_URL}/manager/notSelectedEvents`, {
+      headers: {
+        Authorization: this.jwt_token,
+      },
+    });
+    const data = (await response.json()) as EventProps[];
+    return data;
+  }
+
+  public async addEventToFutureEvents(eventID: number): Promise<boolean> {
+    const url = new URL(`${SERVER_URL}/manager/selectEvent`);
+    url.searchParams.append('eventID', eventID.toString());
+    const response = await fetch(url.toString(), {
+      headers: {
+        Authorization: this.jwt_token,
+      },
+    });
+    return response.ok;
+  }
+
+  public async updateOrganizer(
+    organizerDetail: OrganizerInformation,
+  ): Promise<boolean> {
+    const response = await fetch(`${SERVER_URL}/manager/updateOrganizer`, {
+      headers: {
+        Authorization: this.jwt_token,
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(organizerDetail),
+    });
+    return response.ok;
+  }
+
+  public async getOrganizer(
+    organizerID: number,
+  ): Promise<OrganizerInformation> {
+    const url = new URL(`${SERVER_URL}/manager/organizer`);
+    url.searchParams.append('organizerID', organizerID.toString());
+    const response = await fetch(url.toString(), {
+      headers: { Authorization: this.jwt_token },
+    });
+    const data = await response.json();
+    return data;
+  }
+
+  public async allOrganizers(): Promise<ShortOrganizerInfo[]> {
+    const response = await fetch(`${SERVER_URL}/manager/allOrganizers`, {
+      headers: { Authorization: this.jwt_token },
+    });
+    const data = await response.json();
+    return data;
+  }
+  public async selectEvent(eventID: number): Promise<boolean> {
+    const url = new URL(`${SERVER_URL}/manager/selectEvent`);
+    url.searchParams.append('eventID', eventID.toString());
+    const response = await fetch(url.toString(), {
+      headers: { Authorization: this.jwt_token },
+    });
+
+    return response.ok;
+  }
+}
+
+export default Manager;
