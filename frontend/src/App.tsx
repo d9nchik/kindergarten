1b4b1b621248fd7be9c9efcbbe3fd435eb8d8748
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-11-11 22:39:27 +0200

    fix routing strategy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/*" element={<AuthenticatedRouter />}>
+          <Route path="/" element={<AuthenticatedRouter />}>
             <Route
               path="/*"
               element={
