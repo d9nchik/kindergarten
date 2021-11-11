b4ea79876f61450a756097e5db3657250d61c72d
d9nich <54186666+d9nchik@users.noreply.github.com>
2021-11-11 22:38:49 +0200

    add AllOrganizers, ChangeOrganizer and NotSelectedEvents to Manager component

--- frontend/src/components/manager/Manager.tsx
+++ frontend/src/components/manager/Manager.tsx
@@ -1,7 +1,36 @@
 import React, { FunctionComponent } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import AllOrganizers from './AllOrganizers';
+import ChangeOrganizer from './ChangeOrganizer';
+import { user } from '../../utils/facade';
+import NotSelectedEvents from './NotSelectedEvents';
 
 const Manager: FunctionComponent = () => {
-  return <div>Manager component</div>;
+  const manager = user.getManager();
+  if (!manager) {
+    return <div>Problem with manager account!</div>;
+  }
+
+  return (
+    <div>
+      <Link to="allOrganizers">All organizers</Link>
+      <Link to="notSelectedEvents">New Events</Link>
+      <Routes>
+        <Route
+          path="/allOrganizers"
+          element={<AllOrganizers user={manager} />}
+        />
+        <Route
+          path="/changeOrganizer/:id"
+          element={<ChangeOrganizer user={manager} />}
+        />
+        <Route
+          path="/notSelectedEvents"
+          element={<NotSelectedEvents user={manager} />}
+        />
+      </Routes>
+    </div>
+  );
 };
 
 export default Manager;
